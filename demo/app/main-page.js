27454afb17696dda63e9e224296d9792a9e45dec
Henry Chavez <hchavezv@everis.com>
2018-10-08 01:01:45 -0500

    Update angular version for demo apps and separate script commands

--- demo/app/main-page.js
+++ demo/app/main-page.js
@@ -4,15 +4,17 @@ exports.pageLoaded = function (args) {
 let bar;
 
 let tabSelected = function (args) {
-  if (args.newIndex === 1) {
-    alert('This item has selectable: false, and should be used to perform actions');
-  }
   console.log('tab selected ' + args.newIndex);
+}
+
+let tabPressed = function (args) {
+  alert('This item has selectable: false, and should be used to perform actions');
 }
 
 exports.loaded = function (args) {
   bar = args.object;
   bar.on('tabSelected', tabSelected);
+  bar.on('tabPressed', tabPressed);
 }
 
 exports.changeBackground = () => {
