8ef81745625e5689ff28de12018e8307c17799d5
Henry Chavez <hchavezv@everis.com>
2018-10-10 14:45:50 -0500

    Added tslint to demo projects

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -8,19 +8,19 @@ let bar: BottomNavigation;
 
 const tabSelected = (args: OnTabSelectedEventData) => {
     console.log('tab selected:', args.newIndex);
-}
+};
 
 const tabPressed = (args: OnTabPressedEventData) => {
     console.log('tab pressed:', args.index);
     alert('This item has selectable: false, and should be used to perform actions');
-}
+};
 
 export const loaded = (args) => {
     bar = args.object as BottomNavigation;
     bar.on('tabSelected', tabSelected);
     bar.on('tabPressed', tabPressed);
-}
+};
 
 export const changeBackground = () => {
     bar.backgroundColor = 'black';
-}
+};
